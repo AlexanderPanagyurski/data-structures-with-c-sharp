205e52aa4158835c30e3bb4dddb6085e7074f7d7
AlexanderPanagyurski <alexander.panagyurski@gmail.com>
2023-02-06 19:54:53 +0200

    add advanced

---
+++ Data Structures Advanced (with C#)/02. B-Trees-2-3-Trees-and-Red-Black-Trees-Exercise-Skeleton/01.RedBlackTree/RedBlackTree.cs
@@ -0,0 +1,294 @@
+﻿namespace _01.RedBlackTree
+{
+    using System;
+
+    public class RedBlackTree<T> where T : IComparable
+    {
+        private const bool Red = true;
+        private const bool Black = false;
+        public class Node
+        {
+            public Node(T value)
+            {
+                this.Value = value;
+                this.Color = Red;
+            }
+
+            public T Value { get; set; }
+            public Node Left { get; set; }
+            public Node Right { get; set; }
+            public bool Color { get; set; }
+        }
+
+        public Node root;
+
+        private RedBlackTree(Node node)
+        {
+            PreOrderCopy(node);
+        }
+
+        private void PreOrderCopy(Node node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            this.Insert(node.Value);
+            PreOrderCopy(node.Left);
+            PreOrderCopy(node.Right);
+        }
+
+        public RedBlackTree()
+        {
+
+        }
+
+        public void EachInOrder(Action<T> action)
+        {
+            this.EachInOrder(root, action);
+        }
+
+        private void EachInOrder(Node node, Action<T> action)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            EachInOrder(node.Left, action);
+            action.Invoke(node.Value);
+            EachInOrder(node.Right, action);
+        }
+
+        public RedBlackTree<T> Search(T element)
+        {
+            var node = this.root;
+
+            while (node != null)
+            {
+                if (element.CompareTo(node.Value) == 0)
+                {
+                    break;
+                }
+                else if (element.CompareTo(node.Value) < 0)
+                {
+                    node = node.Left;
+                }
+                else
+                {
+                    node = node.Right;
+                }
+            }
+
+            return new RedBlackTree<T>(node);
+        }
+
+        public void Insert(T element)
+        {
+            root = Insert(root, element);
+            this.root.Color = Black;
+        }
+
+        private Node Insert(Node node, T element)
+        {
+            if (node == null)
+            {
+                return new Node(element);
+            }
+
+            if (element.CompareTo(node.Value) < 0)
+            {
+                node.Left = Insert(node.Left, element);
+            }
+            else
+            {
+                node.Right = Insert(node.Right, element);
+            }
+
+            return FixUp(node);
+        }
+
+        private Node FixUp(Node node)
+        {
+            if (IsRed(node.Right))
+            {
+                node = RotateLeft(node);
+            }
+
+            if (IsRed(node.Left) && IsRed(node.Left.Left))
+            {
+                node = RotateRight(node);
+            }
+
+            if (IsRed(node.Left) && IsRed(node.Right))
+            {
+                FlipColors(node);
+            }
+
+            return node;
+        }
+
+        public void Delete(T key)
+        {
+            if (root == null)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public void DeleteMin()
+        {
+            if (root == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            root = DeleteMin(root);
+
+            if (root != null)
+            {
+                root.Color = Black;
+            }
+        }
+
+        private Node DeleteMin(Node node)
+        {
+            if (node.Left == null)
+            {
+                return null;
+            }
+
+            if (!IsRed(node.Left) && !IsRed(node.Left.Left))
+            {
+                node = MoveRedLeft(node);
+            }
+
+            node.Left = DeleteMin(node.Left);
+
+            return FixUp(node);
+        }
+
+        private Node MoveRedLeft(Node node)
+        {
+            FlipColors(node);
+
+            if (IsRed(node.Right.Left))
+            {
+                node.Right = RotateRight(node.Right);
+                node = RotateLeft(node);
+                FlipColors(node);
+            }
+
+            return node;
+        }
+
+        public void DeleteMax()
+        {
+            if (root == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            root = DeleteMax(root);
+
+            if (root != null)
+            {
+                root.Color = Black;
+            }
+        }
+
+        private Node DeleteMax(Node node)
+        {
+            if (IsRed(node.Left))
+            {
+                node = RotateRight(node);
+            }
+
+            if (node.Right == null)
+            {
+                return null;
+            }
+
+            if (!IsRed(node.Right) && !IsRed(node.Right.Left))
+            {
+                node = MoveRedRight(node);
+            }
+
+            node.Right = DeleteMax(node.Right);
+
+            return FixUp(node);
+        }
+
+        private Node MoveRedRight(Node node)
+        {
+            FlipColors(node);
+
+            if (IsRed(node.Left.Left))
+            {
+                node= RotateLeft(node);
+                FlipColors(node);
+            }
+
+            return node;
+        }
+
+        public int Count()
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+            return Count(root);
+        }
+
+        private int Count(Node node)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+
+            return 1 + Count(node.Left) + Count(node.Right);
+        }
+
+        // Rotation methods
+        private Node RotateLeft(Node node)
+        {
+            var temp = node.Right;
+            node.Right = temp.Left;
+            temp.Left = node;
+            temp.Color = temp.Left.Color;
+            temp.Left.Color = Red;
+
+            return temp;
+        }
+
+        private Node RotateRight(Node node)
+        {
+            var temp = node.Left;
+            node.Left = temp.Right;
+            temp.Right = node;
+            temp.Color = temp.Right.Color;
+            temp.Right.Color = Red;
+
+            return temp;
+        }
+
+        private void FlipColors(Node node)
+        {
+            node.Color = !node.Color;
+            node.Left.Color = !node.Left.Color;
+            node.Right.Color = !node.Right.Color;
+        }
+
+        // Helper methods
+        private bool IsRed(Node node)
+        {
+            if (node == null)
+            {
+                return false;
+            }
+
+            return node.Color == Red;
+        }
+    }
+}
